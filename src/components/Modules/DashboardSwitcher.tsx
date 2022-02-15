02309d04224a4bdb37bedad2a119b5ae5e2c5dbc
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-02-15 23:29:26 +0900

    ドキュメント追加

--- src/components/Modules/DashboardSwitcher.tsx
+++ src/components/Modules/DashboardSwitcher.tsx
@@ -4,6 +4,9 @@ import { AuthContext } from '../../providers/AuthProvider';
 import GoogleLoginButton from '../Elements/Button/GoogleLoginButton';
 import ToDoForm from '../Elements/Form/ToDoForm';
 
+/**
+ * currentUserの存在を検知して、Dashboardの表示を切り替える
+ */
 const DashboardSwitcher: FC = memo(() => {
   const currentUser = useContext(AuthContext);
   return <>{!currentUser ? <GoogleLoginButton /> : <ToDoForm />}</>;
