a74ba7c294399a09047a432958e6e20b0ffb46b3
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-02-15 23:22:39 +0900

    prop型を修正

--- src/components/Atoms/Button/PrimaryButton.tsx
+++ src/components/Atoms/Button/PrimaryButton.tsx
@@ -1,7 +1,10 @@
-import { DOMAttributes, FC, memo } from 'react';
+import { DetailedHTMLProps, FC, FormEventHandler, memo } from 'react';
 
 //Propsの型定義
-type PropsType = {} & DOMAttributes<HTMLButtonElement>;
+type PropsType = {} & DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 const PrimaryButton: FC<PropsType> = memo(({ children, ...attr }) => {
   return <button {...attr}>{children}</button>;
