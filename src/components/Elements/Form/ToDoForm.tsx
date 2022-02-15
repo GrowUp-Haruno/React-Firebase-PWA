7779b8dcac7058f1200b81d27d3859fd2b7e1160
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-02-15 23:30:49 +0900

    書き込み処理を追加

--- src/components/Elements/Form/ToDoForm.tsx
+++ src/components/Elements/Form/ToDoForm.tsx
@@ -1,17 +1,52 @@
-import { ChangeEventHandler, FC, memo, useState } from 'react';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import {
+  ChangeEventHandler,
+  FC,
+  FormEventHandler,
+  memo,
+
+  useContext,
+  useState,
+} from 'react';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { firebaseFirestore } from '../../../service/firebase';
 import PrimaryButton from '../../Atoms/Button/PrimaryButton';
 import PrimaryInputText from '../../Atoms/Input/PrimaryInputText';
 
 const ToDoForm: FC = memo(() => {
-  const [inputValue, setInputValue] = useState<string>();
+  const currentUser = useContext(AuthContext);
+  const [inputValue, setInputValue] = useState<string>('');
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setInputValue(event.target.value);
   };
 
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    try {
+      // リロード抑制
+      event.preventDefault();
+      
+      if (currentUser) {
+        await addDoc(
+          collection(firebaseFirestore, `users/${currentUser.uid}/todos`),
+          {
+            content: inputValue,
+            createdAt: Timestamp.now(),
+            isComplete: false,
+          }
+        );
+        // 書き込み完了後入力内容を削除
+        setInputValue('');
+
+      }
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PrimaryInputText placeholder="TodoName" value={inputValue} onChange={handleChange} />
-      <PrimaryButton>追加</PrimaryButton>
+      <PrimaryButton type="submit">追加</PrimaryButton>
     </form>
   );
 });
