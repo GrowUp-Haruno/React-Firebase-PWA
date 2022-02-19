369718a4782784fe514b586ee5d4942f2553f9e2
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-02-20 05:13:53 +0900

    修正

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,8 +1,8 @@
-import { User } from '@firebase/auth';
 import { createContext, FC } from 'react';
 import { useAuthentication } from '../service/firebaseAuthentication';
+import { currentUserTyep } from '../types/currentUserTyep';
 
-export const AuthContext = createContext<User | undefined>(undefined);
+export const AuthContext = createContext<currentUserTyep>(undefined);
 
 /**
  * Auth情報のプロバイダ
