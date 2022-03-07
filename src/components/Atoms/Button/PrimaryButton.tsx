127e589c4b9903444c9a4730593f62a88672d228
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-03-07 13:57:47 +0900

    ボタン関連を修正

--- src/components/Atoms/Button/PrimaryButton.tsx
+++ src/components/Atoms/Button/PrimaryButton.tsx
@@ -1,22 +1,20 @@
-import { Button } from '@chakra-ui/react';
+import { Button, ButtonProps } from '@chakra-ui/react';
 import { FC, memo, useContext } from 'react';
 import { NowBatchCommitContext } from '../../../providers/NowBatchCommitProvider';
 
-//Propsの型定義
-type PropsType = {
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  type?: 'button' | 'submit' | 'reset' | undefined;
-};
-// type PropsType = {} & DetailedHTMLProps<
-//   React.ButtonHTMLAttributes<HTMLButtonElement>,
-//   HTMLButtonElement
-// >;
-
-const PrimaryButton: FC<PropsType> = memo(({ children, onClick, type }) => {
+const PrimaryButton: FC<ButtonProps> = memo(({ children, onClick, type, leftIcon, variant }) => {
   const { nowBatchCommit } = useContext(NowBatchCommitContext);
 
   return (
-    <Button disabled={nowBatchCommit} onClick={onClick} type={type} backgroundColor='blue.200'>
+    <Button
+      disabled={nowBatchCommit}
+      onClick={onClick}
+      type={type}
+      colorScheme="blue"
+      w="full"
+      variant={variant}
+      leftIcon={leftIcon}
+    >
       {children}
     </Button>
   );
--- src/components/Elements/Button/GoogleLoginButton.tsx
+++ src/components/Elements/Button/GoogleLoginButton.tsx
@@ -1,8 +1,13 @@
 import { FC, memo } from 'react';
+import { FcGoogle } from 'react-icons/fc';
 
 import { signInWithGoogle } from '../../../service/firebaseAuthentication';
 import PrimaryButton from '../../Atoms/Button/PrimaryButton';
 
 export const GoogleLoginButton: FC = memo(() => {
-  return <PrimaryButton onClick={signInWithGoogle}>Google ログイン</PrimaryButton>;
+  return (
+    <PrimaryButton onClick={signInWithGoogle} leftIcon={<FcGoogle />} variant="outline">
+      Google ログイン
+    </PrimaryButton>
+  );
 });
