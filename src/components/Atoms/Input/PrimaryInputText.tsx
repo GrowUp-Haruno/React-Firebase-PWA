cc40cd07166044ac2fb4691c4914111191bdeb36
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-03-07 21:07:55 +0900

    ToDo入力のレイアウトを修正

--- src/components/Atoms/Input/PrimaryInputText.tsx
+++ src/components/Atoms/Input/PrimaryInputText.tsx
@@ -1,11 +1,18 @@
-import { FC, InputHTMLAttributes, memo, useContext } from 'react';
+import { Input, InputProps } from '@chakra-ui/react';
+import { FC, memo, useContext } from 'react';
 import { NowBatchCommitContext } from '../../../providers/NowBatchCommitProvider';
 
 //Propsの型定義
-type PropsType = {} & InputHTMLAttributes<HTMLInputElement>;
+type PropsType = {
+  placeholder: InputProps['placeholder'];
+  value: InputProps['value'];
+  onChange: InputProps['onChange'];
+};
 
-export const PrimaryInputText: FC<PropsType> = memo(({ ...attr }) => {
+export const PrimaryInputText: FC<PropsType> = memo(({ placeholder, value, onChange }) => {
   const { nowBatchCommit } = useContext(NowBatchCommitContext);
-  
-  return <input {...attr} disabled={nowBatchCommit} />;
+
+  return (
+    <Input disabled={nowBatchCommit} placeholder={placeholder} value={value} onChange={onChange} />
+  );
 });
--- src/components/Modules/Todo/ToDoForm.tsx
+++ src/components/Modules/Todo/ToDoForm.tsx
@@ -1,3 +1,4 @@
+import { VStack } from '@chakra-ui/react';
 import { Dispatch, FC, memo, SetStateAction } from 'react';
 
 import { todoGetDataType } from '../../../models/todoGetDataType';
@@ -15,8 +16,10 @@ export const ToDoForm: FC<propsType> = memo(({ todos, setTodos, setUpdateFlag })
   const { inputValue, handleChange, handleSubmit } = useTodoForm(todos, setTodos, setUpdateFlag);
   return (
     <form onSubmit={handleSubmit}>
-      <PrimaryInputText placeholder="TodoName" value={inputValue} onChange={handleChange} />
-      <PrimaryButton type="submit">追加</PrimaryButton>
+      <VStack>
+        <PrimaryInputText placeholder="TodoName" value={inputValue} onChange={handleChange} />
+        <PrimaryButton type="submit">ToDoへ追加</PrimaryButton>
+      </VStack>
     </form>
   );
 });
