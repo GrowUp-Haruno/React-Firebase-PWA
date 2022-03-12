c9fa7ff801f34bce88ab6f29c03c9bdb0237e248
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-03-12 18:11:35 +0900

    コンテキストから取得するように変更

--- src/service/updateLimitCheck.tsx
+++ src/service/updateLimitCheck.tsx
@@ -1,12 +1,10 @@
 import { FirebaseError } from 'firebase/app';
-import { Dispatch } from 'react';
-
-type lastUpdateType = { count: number; time: number };
-
-const InitialLastUpdate: lastUpdateType = {
-  count: 0,
-  time: 0,
-};
+import { Dispatch, SetStateAction } from 'react';
+import {
+  InitialLastUpdate,
+  lastUpdatekeyType,
+  lastUpdateType,
+} from '../providers/LastUpdateProvider';
 
 /**
  * 短時間の変更回数及び前回の更新時間を確認
@@ -16,24 +14,28 @@ const InitialLastUpdate: lastUpdateType = {
  */
 export const updateLimitCheck = (
   lastUpdate: lastUpdateType,
-  setLastUpdate: Dispatch<React.SetStateAction<lastUpdateType>>,
+  setLastUpdate: Dispatch<SetStateAction<lastUpdateType>>,
+  lastUpdatekey: lastUpdatekeyType,
   numberOfLimits: number,
   updateInterval: number,
   firebaseErrorCode: 'changeProfile-error'
 ) => {
   const nowTime = new Date().getTime();
-  if (lastUpdate === InitialLastUpdate) {
+  if (lastUpdate[lastUpdatekey] === InitialLastUpdate[lastUpdatekey]) {
     // プロフィール初変更
-    setLastUpdate({ count: 1, time: nowTime });
+    setLastUpdate({ ...lastUpdate, [lastUpdatekey]: { count: 1, time: nowTime } });
   } else {
     if (
-      lastUpdate.count < numberOfLimits ||
-      lastUpdate.time + updateInterval * 60 * 1000 < nowTime
+      lastUpdate[lastUpdatekey].count < numberOfLimits ||
+      lastUpdate[lastUpdatekey].time + updateInterval * 60 * 1000 < nowTime
     ) {
-      if (lastUpdate.time + updateInterval * 60 * 1000 < nowTime) {
-        setLastUpdate({ count: 1, time: nowTime });
+      if (lastUpdate[lastUpdatekey].time + updateInterval * 60 * 1000 < nowTime) {
+        setLastUpdate({ ...lastUpdate, [lastUpdatekey]: { count: 1, time: nowTime } });
       } else {
-        setLastUpdate({ count: lastUpdate.count + 1, time: nowTime });
+        setLastUpdate({
+          ...lastUpdate,
+          [lastUpdatekey]: { count: lastUpdate[lastUpdatekey].count + 1, time: nowTime },
+        });
       }
     } else {
       // 更新条件の規定値を超えた場合、FirebaseErrorを返す
