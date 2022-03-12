33f5a6123491a01c5ff663fd731368e2b2dd7358
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-03-12 18:07:54 +0900

    更新頻度を管理コンテキストを追加

---
+++ src/providers/LastUpdateProvider.tsx
@@ -0,0 +1,42 @@
+import { createContext, Dispatch, FC, memo, SetStateAction, useState } from 'react';
+import { PicKey } from '../models/UtilityType';
+
+export type lastUpdateObjType = {
+  count: number;
+  time: number;
+};
+
+
+const initialLastUpdateObj: lastUpdateObjType = { count: 0, time: 0 };
+
+export const InitialLastUpdate: lastUpdateType = {
+  changeProfile: initialLastUpdateObj,
+};
+
+type LastUpdateContextType = {
+  lastUpdate: lastUpdateType;
+  setLastUpdate: Dispatch<SetStateAction<lastUpdateType>>;
+};
+
+// 現在の設定
+export type lastUpdateType = {
+  changeProfile: lastUpdateObjType;
+};
+
+// lastUpdateTypeのkey
+export type lastUpdatekeyType = PicKey<lastUpdateType, lastUpdateObjType>;
+
+export const LastUpdateContext = createContext({} as LastUpdateContextType);
+
+/**
+ * Firebaseとの更新頻度を管理
+ */
+export const LastUpdateProvider: FC = memo(({ children }) => {
+  const [lastUpdate, setLastUpdate] = useState<lastUpdateType>(InitialLastUpdate);
+
+  return (
+    <LastUpdateContext.Provider value={{ lastUpdate, setLastUpdate }}>
+      {children}
+    </LastUpdateContext.Provider>
+  );
+});
